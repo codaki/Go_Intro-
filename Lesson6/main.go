a057a0d5f0d9e1e2b47bf8ccd5da8d0498636875
Christopher Iza <cdiza5@espe.edu.ec>
2025-05-29 22:01:14 -0500

    Implicid implementation of interfaces

--- Lesson6/main.go
+++ Lesson6/main.go
@@ -8,7 +8,12 @@ type Animal interface {
 type Dog struct {
 	name string
 }
+type Cat struct {
+	name string
+}
 
+// This method means type Dog implements the interface Animal,
+// but we don't need to explicitly declare that it does so.
 func (f Dog) bark() string {
 	message := "The dog is barking!!!"
 	return message
@@ -17,4 +22,9 @@ func (f Dog) bark() string {
 func main() {
 	var can Animal = Dog{name: "Felipe"}
 	fmt.Println(can.bark())
+
+	//var cat Animal = Cat{name: "Josefino"} is not allowed
+	cat1 := Cat{name: "Josefino"}
+	fmt.Println(cat1.name)
+
 }
